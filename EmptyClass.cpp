6050bb76bb22f4022786c2fda735b27c40b5a88a
Jim Caldwell <jim_caldwell@dell.com>
2020-03-11 16:58:33 -0500

    Create vc19 project plus added emptyclass for staticlib build

---
+++ EmptyClass.cpp
@@ -0,0 +1,12 @@
+#include "EmptyClass.h"
+
+
+
+EmptyClass::EmptyClass()
+{
+}
+
+
+EmptyClass::~EmptyClass()
+{
+}
---
+++ EmptyClass.h
@@ -0,0 +1,9 @@
+#pragma once
+class EmptyClass
+{
+	// this exists so the static library will be generated
+public:
+	EmptyClass();
+	~EmptyClass();
+};
+
